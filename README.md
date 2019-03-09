b37e424bf6ac94956d0e7aeb67cf9cbef65ed71c
OrestF <falchuko@gmail.com>
2019-03-09 14:46:08 +0200

    Updated script suggested by Leonid Kroka

--- README.md
+++ README.md
@@ -13,9 +13,18 @@ Default configs are:
 1.  Copy setup_rails_prototype.sh into folder where you want to create your_rails_app folder
 1. Specify rails version and your app name!
 
-
-
 ### Ruby version must be preinstalled and selected must be preinstalled!
 ```bash
-sh setup_rails_prototype.sh classic 5.2.2 my_awesome_app
+Options:
+    -r         # Rails VERSION
+    -t         # Application type: [api classic]
+    -n         # Application name
+
+Script options:
+    -h         # Show this help message and quit
+    -v         # Show script version number and quit"
+```
+
+```bash
+sh setup_rails_prototype.sh -r 5.2.2 -t api -n my_awesome_app
 ```
--- setup_rails_prototype.sh
+++ setup_rails_prototype.sh
@@ -1,21 +1,45 @@
-#!/usr/bin/env bash
-# bin/bash
+current_version="0.0.1beta#2a345"
+usage="Usage:
+$(basename "$0") [options]
+
+Options:
+    -r         # Rails VERSION
+    -t         # Application type: [api classic]
+    -n         # Application name
+
+Script options:
+    -h         # Show this help message and quit
+    -v         # Show script version number and quit"
+
+while getopts r:t:n::hv option; do
+  case "${option}" in
+    r) VERSION=${OPTARG};;
+    t) TYPE=${OPTARG};;
+    n) APP_NAME=${OPTARG};;
+    h) echo "$usage"
+       exit 0
+       ;;
+    v) echo "$(basename "$0") $current_version"
+       exit 0
+       ;;
+  esac
+done
 
 echo 'Loading...'
 
 gem install bundler
-gem install rails -v $2
+gem install rails -v $VERSION
 
-if [ $1 == "api" ]
+if [ x$TYPE = x"api" ]
 then
     echo 'Rails API setup started'
     git clone https://github.com/OrestF/rails_api_prototype.git
-    rails new $3 --api -d postgresql -T --skip-coffee -m ./rails_api_prototype/template.rb
+    rails _$VERSION\_ new $APP_NAME --api -d postgresql -T --skip-coffee -m ./rails_api_prototype/template.rb
     rm -rf ./rails_api_prototype
 else
     echo 'Rails setup started'
     git clone https://github.com/OrestF/rails_prototype.git
-    rails new $3 -d postgresql -T --skip-coffee -m ./rails_prototype/template.rb
+    rails _$VERSION\_ new $APP_NAME -d postgresql -T --skip-coffee -m ./rails_prototype/template.rb
     rm -rf ./rails_prototype
 fi
 echo 'Done!'
